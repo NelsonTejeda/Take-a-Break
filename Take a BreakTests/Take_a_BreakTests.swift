4ce3ef50d0453e26bb9a5662038ba4c8f5330e7d
Nelson Tejeda <nelsonjesustejeda@gmail.com>
2020-09-17 17:32:20 -0400

    Initial Commit

---
+++ Take a BreakTests/Take_a_BreakTests.swift
@@ -0,0 +1,34 @@
+//
+//  Take_a_BreakTests.swift
+//  Take a BreakTests
+//
+//  Created by nelson tejeda on 9/17/20.
+//  Copyright © 2020 Nelson Tejeda. All rights reserved.
+//
+
+import XCTest
+@testable import Take_a_Break
+
+class Take_a_BreakTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
